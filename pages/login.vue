0711aea02859a2b34fc378f190a1fefeb44087eb
vitaminwater <stantin04@gmail.com>
2021-11-30 08:22:05 +0100

    added login from SuperGreenLive2

---
+++ pages/login.vue
@@ -0,0 +1,119 @@
+<!--
+      Copyright (C) 2021  SuperGreenLab <towelie@supergreenlab.com>
+      Author: Constantin Clauzel <constantin.clauzel@gmail.com>
+
+      This program is free software: you can redistribute it and/or modify
+      it under the terms of the GNU General Public License as published by
+      the Free Software Foundation, either version 3 of the License, or
+      (at your option) any later version.
+
+      This program is distributed in the hope that it will be useful,
+      but WITHOUT ANY WARRANTY without even the implied warranty of
+      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+      GNU General Public License for more details.
+
+      You should have received a copy of the GNU General Public License
+      along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ -->
+
+<template>
+  <section :id="$style.container">
+    <form @submit='loginHandler'>
+      <div :id='$style.body'>
+        <div :id='$style.title'>S<span :id='$style.green'>G</span>L LOGIN</div>
+        <input type='text' placeholder='Login' v-model='login' @change=''/>
+        <input type='password' placeholder='Password' v-model='password' />
+        <div :id='$style.app'>No account yet? create one on the <a target='_blank' href='https://www.supergreenlab.com/app'>sgl app</a></div>
+        <span :id='$style.error' v-if='error'>Wrong login/password</span>
+        <div :id='$style.button'>
+          <button @click='loginHandler'>LOGIN</button>
+        </div>
+      </div>
+    </form>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      login: '',
+      password: '',
+    }
+  },
+  watch: {
+    loggedIn(val) {
+      if (val == true) {
+        this.$router.replace('/')
+      }
+    },
+  },
+  methods: {
+    loginHandler(e) {
+      e.preventDefault()
+      e.stopPropagation()
+      const { login, password } = this.$data
+      this.$store.dispatch('auth/login', { login, password })
+      return false
+    },
+  },
+  computed: {
+    loggedIn() {
+      return this.$store.state.auth.loggedIn
+    },
+    error() {
+      return this.$store.state.auth.error
+    },
+  },
+}
+</script>
+
+<style module lang=stylus>
+#container
+  display: flex
+  height: 100vh
+  justify-content: center
+  align-items: center
+
+#body
+  display: flex
+  flex-direction: column
+
+#body > input
+  margin: 3pt 0
+  padding: 3pt 6pt
+
+#green
+  color: #3bb30b
+
+#title
+  color: #454545
+  font-weight: bold
+
+#button
+  display: flex
+  justify-content: flex-end
+  align-items: flex-end
+  padding: 15pt 0 0 0
+
+#button > button
+  border: none
+  color: white
+  border-radius: 2.5px
+  background-color: #3bb30b
+  padding: 2pt 20pt
+  cursor: pointer
+
+#button > button:hover
+  background-color: #4bc31b
+
+#button > button:active
+  background-color: #2ba300
+
+#error
+  color: red
+
+#app
+  color: #454545
+
+</style>
---
+++ store/auth.js
@@ -0,0 +1,89 @@
+/*
+ * Copyright (C) 2021  SuperGreenLab <towelie@supergreenlab.com>
+ * Author: Constantin Clauzel <constantin.clauzel@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+import Vue from 'vue'
+import axios from 'axios'
+
+import { loadFromStorage, saveToStorage } from '~/lib/client-side.js'
+
+const STORAGE_ITEM='auth'
+const API_URL=process.env.API_URL
+const RPI_URL=process.env.RPI_URL
+
+export const state = () => {
+  let defaults = {
+    token: null,
+    error: false,
+    loading: false,
+    loggedIn: false,
+  };
+  return defaults
+};
+
+const storeState = (state) => {
+  saveToStorage(STORAGE_ITEM, JSON.stringify(state))
+}
+
+export const actions = {
+  async nuxtClientInit({ commit }) {
+    const saved = loadFromStorage(STORAGE_ITEM)
+    if (saved) {
+      commit('setState', JSON.parse(saved))
+    }
+  },
+  async login({ commit, dispatch }, { login, password }) {
+    commit('setLoading', true)
+    try {
+      const resp = await axios.post(`${API_URL}/login`, {
+        handle: login,
+        password,
+      })
+      const token = resp.headers['x-sgl-token']
+
+      commit('setToken', token)
+      commit('setLoggedIn', true)
+    } catch(e) {
+      commit('setError', true)
+    }
+    commit('setLoading', false)
+  },
+}
+
+export const mutations = {
+  setState(state, newState) {
+    Object.assign(state, newState)
+  },
+  setToken(state, token) {
+    state.token = token
+    storeState(state)
+  },
+  setLoggedIn(state, loggedIn) {
+    state.loggedIn = loggedIn
+    storeState(state)
+  },
+  setLoading(state, loading) {
+    state.loading = loading
+  },
+  setError(state, error) {
+    state.error = error
+  },
+}
+
+export const getters = {
+}
